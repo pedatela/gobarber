3be168bd4e2ee0a3e7a728397d97ea996239ae92
Lucas Pedatela <lucaspedatela@MacBook-Pro-de-Lucas.local>
2020-01-11 12:59:02 -0300

    Configurando Rotas

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from '../pages/Dashboard'
+import Profile from '../pages/Profile'
+
+import SignUp from '../pages/SignUp'
+import SignIn from '../pages/SignIn'
+
+export default function Routes() {
+  <Switch>
+    <Route path='/' exact component={SignIn} />
+    <Route path='/register' component={SignUp} />
+    <Route path='/profile' component={Profile} />
+    <Route path='/dashboard' component={Dashboard} />
+
+  </Switch>
+}
